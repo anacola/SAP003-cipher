bf081a0e9bd70d4e5d298485a4465fd4a4e974f7
anacola <anac.cola@gmail.com>
2019-08-26 17:24:16 -0300

    Cipher.js cifrando e decifrando: Maiusculas, minusculas e simbolos

--- src/cipher.js
+++ src/cipher.js
@@ -7,14 +7,14 @@ window.cipher = {
 function encode(deslocamento, mensagem) {
   let resultado = "" ;
   let formula;
-    for (let i=0; mensagem.length >i; i++) {
+  for (let i=0; mensagem.length >i; i++) {
     if (mensagem.charCodeAt(i) >=65 && mensagem.charCodeAt(i) <=90) {
       formula = ((mensagem.charCodeAt(i) -65 + deslocamento)% 26)+65;
       resultado += String.fromCharCode(formula);
-    } else if (mensagem.charCodeAt(i) >=97 && mensagem.charCodeAt(i) <=122){
+    } else if (mensagem.charCodeAt(i) >=97 && mensagem.charCodeAt(i) <=122) {
       formula = ((mensagem.charCodeAt(i) -97 + deslocamento)% 26)+97;
       resultado += String.fromCharCode(formula);
-    } else if (mensagem.charCodeAt(i) >=32 && mensagem.charCodeAt(i)<=64){
+    } else if (mensagem.charCodeAt(i) >=32 && mensagem.charCodeAt(i)<=64) {
       formula = (mensagem.charCodeAt(i));
       resultado += String.fromCharCode(formula);
     }
@@ -24,14 +24,15 @@ function encode(deslocamento, mensagem) {
 
 function decode(deslocamento, mensagem) {
   let decoderesult = "" ;
+  let formula;
   for (let i=0; mensagem.length >i; i++) {
     if (mensagem.charCodeAt(i)>=65 && mensagem.charCodeAt(i)<=90) {
-      let formula = ((mensagem.charCodeAt(i) - 90 - deslocamento)% 26)+90;
+      formula = ((mensagem.charCodeAt(i) - 90 - deslocamento)% 26)+90;
       decoderesult += String.fromCharCode(formula);
-    } else if (mensagem.charCodeAt(i)>=97 && mensagem.charCodeAt(i) <=122){
+    } else if (mensagem.charCodeAt(i)>=97 && mensagem.charCodeAt(i) <=122) {
       formula = ((mensagem.charCodeAt(i) - 122 - deslocamento)%26)+122;
       decoderesult += String.fromCharCode(formula);
-    } else if (mensagem.charCodeAt(i) >=32 && mensagem.charCodeAt(i)<=64){
+    } else if (mensagem.charCodeAt(i) >=32 && mensagem.charCodeAt(i)<=64) {
       formula = (mensagem.charCodeAt(i));
       decoderesult += String.fromCharCode(formula);
     }  
